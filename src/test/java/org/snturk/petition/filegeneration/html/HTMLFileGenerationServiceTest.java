3b57490bb9b5c0dbaa0d2829d41c535d61c31349
Muratcan Şentürk <muratcan.senturk@ozguryazilim.com.tr>
2023-04-02 13:04:43 +0300

    feat: Petition Refactor
    * Tests optimized to work with after refactor
    
    Refs: ?

--- src/test/java/org/snturk/petition/filegeneration/html/HTMLFileGenerationServiceTest.java
+++ src/test/java/org/snturk/petition/filegeneration/html/HTMLFileGenerationServiceTest.java
@@ -7,11 +7,15 @@ import org.snturk.petition.ImmutablePetitionModel;
 import org.snturk.petition.PetitionModel;
 import org.snturk.petition.enums.PetitionType;
 import org.snturk.petition.model.Person;
+import org.snturk.petition.signature.ImmutableSignatureInfo;
+import org.snturk.petition.signature.SignatureContext;
 import org.snturk.petition.signature.SignatureService;
+import org.snturk.petition.signature.TextBasedSignature;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -30,13 +34,18 @@ public class HTMLFileGenerationServiceTest {
                 .idGenerator(this::generateId)
                 .name("Test Petition")
                 .title("Test Petition Title")
-                .addIssuers(issuer)
+                .addPetitioners(issuer)
                 .content(testContent)
                 .type(PetitionType.REQUEST)
                 .issuedDate(issueDate)
                 .build();
 
-        var petitionModel = signatureService.performSign(petitionModelTemp, issuer);
+        var signatureInfo = ImmutableSignatureInfo.builder()
+                .signatureType(new TextBasedSignature())
+                .signedAt(LocalDateTime.now())
+                .build();
+        var signatureContext = new SignatureContext(issuer, signatureInfo);
+        var petitionModel = signatureService.performSign(petitionModelTemp, signatureContext);
 
         // Then we can generate the file
         var path = new File("src/test/resources/generated.html").toPath();
@@ -45,7 +54,8 @@ public class HTMLFileGenerationServiceTest {
         // Now we can check the generated file
         var generatedFile = FileUtils.readFileToString(new File("src/test/resources/generated.html"));
         var expectedFile = FileUtils.readFileToString(new File("src/test/resources/test1ExpectedResult.html"));
-        // Compare without indentation
+        // Compare without indentation and time differences
+        expectedFile = expectedFile.replace("{{signedAt}}", signatureInfo.getSignedAt().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
         assertEquals(expectedFile.replaceAll("\\s+", ""), generatedFile.replaceAll("\\s+", ""));
     }
 
@@ -60,15 +70,21 @@ public class HTMLFileGenerationServiceTest {
                 .idGenerator(this::generateId)
                 .name("Test Petition")
                 .title("Test Petition Title")
-                .addIssuers(issuer1)
-                .addIssuers(issuer2)
+                .addPetitioners(issuer1)
+                .addPetitioners(issuer2)
                 .content(testContent)
                 .type(PetitionType.REQUEST)
                 .issuedDate(issueDate)
                 .build();
 
-        var petitionModel = signatureService.performSign(petitionModelTemp, issuer1);
-        petitionModel = signatureService.performSign(petitionModel, issuer2);
+        var signatureInfo = ImmutableSignatureInfo.builder()
+                .signatureType(new TextBasedSignature())
+                .signedAt(LocalDateTime.now())
+                .build();
+        var signatureContext1 = new SignatureContext(issuer1, signatureInfo);
+        var signatureContext2 = new SignatureContext(issuer2, signatureInfo);
+        var petitionModel = signatureService.performSign(petitionModelTemp, signatureContext1);
+        petitionModel = signatureService.performSign(petitionModel, signatureContext2);
 
         // Then we can generate the file
         var path = new File("src/test/resources/generated.html").toPath();
@@ -78,6 +94,7 @@ public class HTMLFileGenerationServiceTest {
         var generatedFile = FileUtils.readFileToString(new File("src/test/resources/generated.html"));
         var expectedFile = FileUtils.readFileToString(new File("src/test/resources/test2ExpectedResult.html"));
         // Compare without indentation
+        expectedFile = expectedFile.replace("{{signedAt}}", signatureInfo.getSignedAt().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
         assertEquals(expectedFile.replaceAll("\\s+", ""), generatedFile.replaceAll("\\s+", ""));
     }
 
--- src/test/java/org/snturk/petition/signature/SignatureServiceTest.java
+++ src/test/java/org/snturk/petition/signature/SignatureServiceTest.java
@@ -29,14 +29,20 @@ public class SignatureServiceTest {
                 .idGenerator(this::generateId)
                 .name("Test Petition")
                 .title("Test Petition Title")
-                .addIssuers(issuer)
+                .addPetitioners(issuer)
                 .content(testContent)
                 .type(PetitionType.REQUEST)
                 .issuedDate(issueDate)
                 .build();
-        var signedPetition = signatureService.performSign(petitionModelTemp, issuer);
+
+        var signatureInfo = ImmutableSignatureInfo.builder()
+                .signatureType(new TextBasedSignature())
+                .signedAt(LocalDateTime.now())
+                .build();
+        var signatureContext = new SignatureContext(issuer, signatureInfo);
+        var signedPetition = signatureService.performSign(petitionModelTemp, signatureContext);
         assertEquals(1, signedPetition.getSignatures().size());
-        assertEquals(1, signedPetition.getSigners().length, signedPetition.getSignatures().get(0).getIssuers().length);
+        assertEquals(1, signedPetition.getPetitioners().size(), signedPetition.getSignatures().size());
     }
 
     @Test @DisplayName("Perform Sign Test with Multiple Signers")
@@ -49,32 +55,44 @@ public class SignatureServiceTest {
                 .idGenerator(this::generateId)
                 .name("Test Petition")
                 .title("Test Petition Title")
-                .addIssuers(issuer1)
-                .addIssuers(issuer2)
+                .addPetitioners(issuer1)
+                .addPetitioners(issuer2)
                 .content(testContent)
                 .type(PetitionType.REQUEST)
                 .issuedDate(issueDate)
                 .build();
-        var signedPetition = signatureService.performSign(petitionModelTemp, issuer1);
+
+        var signatureInfo = ImmutableSignatureInfo.builder()
+                .signatureType(new TextBasedSignature())
+                .signedAt(LocalDateTime.now())
+                .build();
+        var signatureContext = new SignatureContext(issuer1, signatureInfo);
+        var signedPetition = signatureService.performSign(petitionModelTemp, signatureContext);
         assertEquals(1, signedPetition.getSignatures().size());
-        assertEquals(2, signedPetition.getSigners().length, signedPetition.getSignatures().get(0).getIssuers().length);
+        assertEquals(1, signedPetition.getPetitioners().size(), signedPetition.getSignatures().size());
     }
 
     @Test @DisplayName("Perform Sign Test with Invalid Date")
     void shouldPerformSignWithInvalidDate() throws IOException {
         var issuer = new Person("Muratcan", "Senturk");
         var testContent = FileUtils.readFileToString(new File("src/test/resources/testContent.txt"));
-        var issueDate = LocalDateTime.now().plusDays(1);
+        var issueDate = LocalDateTime.now();
         var petitionModelTemp = ImmutablePetitionModel.builder()
                 .idGenerator(this::generateId)
                 .name("Test Petition")
                 .title("Test Petition Title")
-                .addIssuers(issuer)
+                .addPetitioners(issuer)
                 .content(testContent)
                 .type(PetitionType.REQUEST)
                 .issuedDate(issueDate)
                 .build();
-        assertThrows(InvalidSignatureInfoException.class, () -> signatureService.performSign(petitionModelTemp, issuer));
+
+        var signatureInfo = ImmutableSignatureInfo.builder()
+                .signatureType(new TextBasedSignature())
+                .signedAt(LocalDateTime.now().minusDays(2))
+                .build();
+        var signatureContext = new SignatureContext(issuer, signatureInfo);
+        assertThrows(InvalidSignatureInfoException.class, () -> signatureService.performSign(petitionModelTemp, signatureContext));
     }
 
     @Test @DisplayName("Perform Sign Test with Invalid Signer")
@@ -86,12 +104,18 @@ public class SignatureServiceTest {
                 .idGenerator(this::generateId)
                 .name("Test Petition")
                 .title("Test Petition Title")
-                .addIssuers(issuer)
+                .addPetitioners(issuer)
                 .content(testContent)
                 .type(PetitionType.REQUEST)
                 .issuedDate(issueDate)
                 .build();
-        assertThrows(InvalidSignatureInfoException.class, () -> signatureService.performSign(petitionModelTemp, new Person("John", "Doe")));
+
+        var signatureInfo = ImmutableSignatureInfo.builder()
+                .signatureType(new TextBasedSignature())
+                .signedAt(LocalDateTime.now())
+                .build();
+        var wrongSignatureContext = new SignatureContext(new Person("John", "Doe"), signatureInfo);
+        assertThrows(InvalidSignatureInfoException.class, () -> signatureService.performSign(petitionModelTemp, wrongSignatureContext));
     }
 
     private String generateId(PetitionModel model) {
